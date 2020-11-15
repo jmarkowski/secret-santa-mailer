51ceeed2d21b280d839d6bb78a29f662a688782a
Jan Markowski <jan@markowski.ca>
2020-11-15 15:59:50 -0500

    Drop unused 'wish_list' property from santa

--- santa.py
+++ santa.py
@@ -1,8 +1,7 @@
 class Santa(object):
-    def __init__(self, name, email, wish_list=[]):
+    def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.wish_list = wish_list
         self.recipient = None
 
     def set_recipient(self, santa):
