05d2c8061b4555744ad21fdb7fcf86dda5c2739e
Jan Markowski <jan@markowski.ca>
2022-11-30 19:45:29 -0500

    Amalgamate entities into the executing script now with the config script's entity dependencies removed

--- send-letters.py
+++ send-letters.py
@@ -2,13 +2,68 @@
 import argparse
 import random
 import re
-
-from santa import Santa
-from letter import Letter
+import smtplib
+from functools import total_ordering
 
 
 class SecretSantaError(Exception):
     pass
+
+
+@total_ordering
+class Santa(object):
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.recipient = None
+
+    def __eq__(self, other):
+        return self.name.lower() == other.name.lower()
+
+    def __lt__(self, other):
+        assert isinstance(other, Santa)
+        return self.name.lower() < other.name.lower()
+
+    def __str__(self):
+        if isinstance(self.recipient, Santa):
+            return f'{self.name:12} -> {self.recipient.name}'
+
+        return f'{self.name:12}'
+
+
+class Letter():
+    def __init__(self, from_name, from_email, subject, body):
+        self.from_name = from_name
+        self.from_email = from_email
+        self.subject = subject
+        self.body = body
+
+    def get_email_message(self, santa):
+        message = \
+            f'From: {self.from_name} <{self.from_email}>\n' \
+            f'To: {santa.name} <{santa.email}>\n' \
+            f'Subject: {self.subject}\n\n' \
+            f'{self.body}\n'
+
+        message = message.replace('{santa}', santa.name)
+        message = message.replace('{recipient}', santa.recipient.name)
+
+        return message
+
+    def send(self, santa, smtp_settings):
+
+        message = self.get_email_message(santa)
+
+        try:
+            server = smtplib.SMTP(smtp_settings['host'], smtp_settings['port'])
+            server.starttls()
+            server.login(smtp_settings['username'], smtp_settings['password'])
+            server.sendmail(self.from_email, [santa.email], message.encode('utf-8'))
+            server.close()
+
+            print('Successfully mailed letter to {}!'.format(santa.name))
+        except Exception as e:
+            print('Error: Failed to mail letter: {}'.format(e))
 
 
 def is_santa_list_compatible(santas_lst, incompatibles):
